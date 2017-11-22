5bc7f87b61b7849fc4aadb833512eeb39f3956d9
HoushCE29 <HoushCE29@uww.edu>
2017-11-21 20:00:54 -0600

    Update model.php

--- php/model.php
+++ php/model.php
@@ -1,43 +1,72 @@
 <?php
 
- ### THESE FUNCTIONS RETRIEVE RESULTS WITH pdo_connect.php ###
+#############################################################
+# These functions handle directly with the database queries #
+#############################################################
 
 /**
   Returns all results as an indexed array of associative arrays
 */
-function getAllRecords($sql, $data=NULL){
-      global $db;
-        // Prepare SQL statement
-        $stm = $db->prepare($sql);
-        // Execute SQL statement
-        $stm->execute($data);
-        // Fetch all the records
-        $results = $stm->fetchAll(PDO::FETCH_ASSOC);
-        // return values
-        return $results;
+function getAll($sql, $data=NULL){
+  global $db;
+  $stm = $db->prepare($sql);
+  $stm->execute($data);
+  $results = $stm->fetchAll(PDO::FETCH_ASSOC);
+  return $results;
 }
 
 /**
   Returns a single result at a time to save memory.
 */
-function getRecord($sql, $data=NULL){
-      global $db;
-        // Prepare SQL statement
-        $stm = $db->prepare($sql);
-        // Execute SQL statement
-        $stm->execute($data);
-        // Fetch all the records
-        $result = $stm->fetch(PDO::FETCH_ASSOC);
-        // return values
-        return $result;
+function get($sql, $data=NULL){
+  global $db;
+  $stm = $db->prepare($sql);
+  $stm->execute($data);
+  $result = $stm->fetch(PDO::FETCH_ASSOC);
+  return $result;
 }
 
 /**
-  Returns the getAllRecords() function as a JSON object.
+  Returns the getAll() function as a JSON object.
 */
-function getJSON($sql){
-  $results = getAllRecords($sql);
+function getJSON($sql, $data=NULL){
+  $results = getAll($sql, $data);
   return json_encode($results);
 }
+####################################################
 
+##########################################################################
+# These functions handle raw form data and  echo back JSON object arrays #
+##########################################################################
+
+//TODO: CHANGE POST VARS
+function getByClub(){
+  $data = array(':goals' => 0, ':disc' => 0, ':xp' => 0);
+  if(isset($_POST['goals'])&&sizeof($_POST['goals'])>0){
+    $data[':goals'] = $_POST['goals'];
+  }
+  if(isset($_POST['disc'])&&sizeof($_POST['disc'])>0){
+    $data[':disc'] = $_POST['disc'];
+  }
+  if(isset($_POST['xp'])&&sizeof($_POST['xp'])>0){
+    $data[':xp'] = $_POST['xp'];
+  }
+  $sql = "CALL findClubByStats(:goals, :disc, :xp);";
+  echo getJSON($sql, $data);
+}
+
+
+function getByPlayer(){
+
+}
+
+
+function getByAchievement(){
+
+}
+
+
+function getByMisc(){
+
+}
 ?>
