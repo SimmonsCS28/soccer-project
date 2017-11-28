93659b17ff5c438541e70c8b8057f051b071eb70
Cole Simmons <SimmonsCS28@gmail.com>
2017-11-28 17:59:49 -0600

    Player Search Form Modification & Ach Form Creation
    Modified the player search form and created the achievements form

---
+++ src/app/achievement/achievement-search/achievement-search.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-achievement-search',
+  templateUrl: './achievement-search.component.html',
+  styleUrls: ['./achievement-search.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AchievementSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/player/player-search/player-search.component.ts
+++ src/app/player/player-search/player-search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-player-search',
@@ -7,10 +9,15 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class PlayerSearchComponent implements OnInit {
+@ViewChild('playerSearchForm') public playerSearchForm: NgForm;
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public playerSearchSubmit (){
+    console.log(this.playerSearchForm.value.noExpPref);
+  }
 }
