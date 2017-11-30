41a4739e29bda120f5280871a4b4a40f5c3cb47d
Cole Simmons <SimmonsCS28@gmail.com>
2017-11-30 10:44:52 -0600

    Player search http request
    Setting up http request for player search

--- src/app/player/player-search/player-search.component.ts
+++ src/app/player/player-search/player-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-player-search',
@@ -9,15 +10,22 @@ import { NgForm } from '@angular/forms';
   encapsulation: ViewEncapsulation.None
 })
 export class PlayerSearchComponent implements OnInit {
-@ViewChild('playerSearchForm') public playerSearchForm: NgForm;
 
+  @ViewChild('playerSearchForm') public playerSearchForm: NgForm;
+  public url: string = "http://washington.uww.edu/cs366/houshce29/db/data_fetch.php";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
-  public playerSearchSubmit (){
+  public playerSearchSubmit() {
     console.log(this.playerSearchForm.value.noExpPref);
+    this.http.get(this.url).subscribe(response => {
+      console.log(response);
+    }),
+      (err: HttpErrorResponse) => {
+        console.log(err);
+      };
   }
 }
